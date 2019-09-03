8a806e15f7b3bf79778411ee40cb762512897ee1
eriKapeyyy <48813858+eriKapeyyy@users.noreply.github.com>
2019-09-03 17:06:32 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,12 +3,20 @@ const client = new Discord.Client()
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
-})
+});
 
+/*attendance*/
 client.on('message', msg => {
   if(msg.content === '!present') {
     msg.reply('You already have attendance for today!')
   }
-})
+});
+
+/*new member server*/
+client.on('guildMemberAdd', member => {
+  const channel = member.guild.channels.find(ch => ch.name === 'member-log');
+  if (!channel) return;
+  channel.send(`Welcome to the server, ${member}`);
+});
 
 client.login(process.env.BOT_TOKEN)
