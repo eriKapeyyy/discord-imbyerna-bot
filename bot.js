4e4cbc0413e7434f25b7387e8adbd54f8c02e738
eriKapeyyy <48813858+eriKapeyyy@users.noreply.github.com>
2019-09-04 14:52:10 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,7 +27,7 @@ client.on('message', msg => {
   else if(msg.content.toLowerCase() === 'tangina' || msg.content.toLowerCase() === 'tang ina' || msg.content.toLowerCase() === 'tangina mo' || msg.content.toLowerCase() === 'tang ina mo') {
     msg.channel.send('Hoy tangina ka bawal magmura rito');
   }
-  else if(msg.content.toLower() === 'mahal niya ba ako?' || msg.content.toLowerCase() === 'mahal ba ko?' || msg.content.toLowerCase() === 'mahal niya ba ko' || msg.content.toLowerCase() === 'mahal ba ko' || msg.content.toLowerCase() === 'mahal nya ba ko') {
+  else if(msg.content.toLowerCase() === 'mahal niya ba ako?' || msg.content.toLowerCase() === 'mahal ba ko?' || msg.content.toLowerCase() === 'mahal niya ba ko' || msg.content.toLowerCase() === 'mahal ba ko' || msg.content.toLowerCase() === 'mahal nya ba ko') {
     msg.channel.send('Di ka niya mahal');
   }
   else if(msg.content.toLowerCase() === 'iloveyou' || msg.content.toLowerCase() === 'mahal kita' || msg.content.toLowerCase() === 'i love you' || msg.content.toLowerCase() === 'ily') {
